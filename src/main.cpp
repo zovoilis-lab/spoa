64d0edd268d82099df35c7a5ed39e4d5d9e439f1
rvaser <robert.vaser@gmail.com>
2016-03-16 17:11:18 +0100

    Removed some stuff

--- src/main.cpp
+++ src/main.cpp
@@ -27,31 +27,8 @@ int main(int argc, char** argv) {
         alignment->align_sequence_to_graph();
         alignment->backtrack();
 
-        continue;
-
         const auto& node_ids = alignment->alignment_node_ids();
         const auto& seq_ids = alignment->alignment_seq_ids();
-
-        if (seq_ids.empty() == false) {
-            for (const auto& id: seq_ids) {
-                if (id == -1) {
-                    printf("-");
-                } else {
-                    printf("%c", reads[i]->data()[id] + 'A');
-                }
-            }
-            printf("\n");
-            for (const auto& id: node_ids) {
-                if (id == -1) {
-                    printf("-");
-                } else {
-                    printf("%c", graph->node(id)->letter() + 'A');
-                }
-            }
-            printf("\n");
-        }
-
-        continue;
 
         graph->add_alignment(node_ids, seq_ids, reads[i]->data());
         //graph->print();
@@ -62,13 +39,13 @@ int main(int argc, char** argv) {
     for (const auto& alignment_str: msa) {
         fprintf(stderr, "%s\n", alignment_str.c_str());
     }
-    fprintf(stderr, "\n");
+    fprintf(stderr, "\n");*/
     std::string consensus = graph->generate_consensus();
     fprintf(stderr, "Consensus (%zu)\n", consensus.size());
     for (const auto& c: consensus) {
-        fprintf(stderr, "%c", c + 'A');
+        fprintf(stderr, "%c", c);
     }
-    fprintf(stderr, "\n");*/
+    fprintf(stderr, "\n");
 
     return 0;
 }
