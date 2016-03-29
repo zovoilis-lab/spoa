eab9d92f733842eda43e5164d79b72834b594996
rvaser <robert.vaser@gmail.com>
2016-03-29 12:31:37 +0200

    Changed the way weights are added to edges

--- src/graph.cpp
+++ src/graph.cpp
@@ -183,23 +183,16 @@ void Graph::add_alignment(std::shared_ptr<Alignment> alignment, const std::strin
     int32_t start_node_id = this->add_sequence(sequence, weights, 0, valid_seq_ids.front());
     int32_t head_node_id = tmp == num_nodes_ ? -1 : num_nodes_ - 1;
 
-    //fprintf(stderr, "%d %d\n", start_node_id, head_node_id);
-
     int32_t tail_node_id = this->add_sequence(sequence, weights, valid_seq_ids.back() + 1, sequence.size());
 
-    //fprintf(stderr, "%d\n", tail_node_id);
-
     int32_t new_node_id = -1;
-    float prev_weight = head_node_id == -1 ? -1 : weights[valid_seq_ids.front() - 1];
 
     for (uint32_t i = 0; i < seq_ids.size(); ++i) {
-        //fprintf(stderr, "%d| %d %d %d\n", seq_ids[i], start_node_id, head_node_id, new_node_id);
         if (seq_ids[i] == -1) {
             continue;
         }
 
         char letter = sequence[seq_ids[i]];
-        //fprintf(stderr, "%c\n", letter);
         if (node_ids[i] == -1) {
             new_node_id = this->add_node(letter);
 
@@ -239,16 +232,14 @@ void Graph::add_alignment(std::shared_ptr<Alignment> alignment, const std::strin
         }
 
         if (head_node_id != -1) {
-            this->add_edge(head_node_id, new_node_id, prev_weight);
+            this->add_edge(head_node_id, new_node_id, weights[seq_ids[i]]);
         }
 
         head_node_id = new_node_id;
-        prev_weight = weights[seq_ids[i]];
     }
-    //fprintf(stderr, "%d %d %d\n", start_node_id, head_node_id, new_node_id);
 
     if (tail_node_id != -1) {
-        this->add_edge(head_node_id, tail_node_id, prev_weight);
+        this->add_edge(head_node_id, tail_node_id, weights[valid_seq_ids.back()+1]);
     }
 
     ++num_sequences_;
@@ -272,7 +263,7 @@ int32_t Graph::add_sequence(const std::string& sequence, const std::vector<float
     uint32_t node_id;
     for (uint32_t i = begin + 1; i < end; ++i) {
         node_id = this->add_node(sequence[i]);
-        this->add_edge(node_id - 1, node_id, weights[i - 1]);
+        this->add_edge(node_id - 1, node_id, weights[i]);
     }
 
     return first_node_id;
