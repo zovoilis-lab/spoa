e2cddb0a6d6953923af334829728551c07e4af0c
Erik Garrison <erik.garrison@gmail.com>
2020-05-31 11:13:51 +0200

    write GFAv1 of POA graph

--- src/main.cpp
+++ src/main.cpp
@@ -36,9 +36,10 @@ int main(int argc, char** argv) {
     std::uint8_t result = 0;
 
     std::string dot_path = "";
+    bool write_gfa = false;
 
     int opt;
-    while ((opt = getopt_long(argc, argv, "m:n:g:e:q:c:l:r:d:h", options, nullptr)) != -1) {
+    while ((opt = getopt_long(argc, argv, "m:n:g:e:q:c:l:r:d:fh", options, nullptr)) != -1) {
         switch (opt) {
             case 'm': m = atoi(optarg); break;
             case 'n': n = atoi(optarg); break;
@@ -49,6 +50,7 @@ int main(int argc, char** argv) {
             case 'l': algorithm = atoi(optarg); break;
             case 'r': result = atoi(optarg); break;
             case 'd': dot_path = optarg; break;
+            case 'f': write_gfa = true; break;
             case 'v': std::cout << version << std::endl; return 0;
             case 'h': help(); return 0;
             default: return 1;
@@ -118,7 +120,17 @@ int main(int argc, char** argv) {
         }
     }
 
-    if (result == 0) {
+    if (write_gfa) {
+        // force consensus genertion for graph annotation
+        std::string consensus = graph->generate_consensus();
+        // save sequence names for graph path labeling
+        std::vector<std::string> sequence_names;
+        for (auto& s : sequences) {
+            sequence_names.push_back(s->name());
+        }
+        // write the graph
+        graph->print_gfa(std::cout, sequence_names);
+    } else if (result == 0) {
         std::string consensus = graph->generate_consensus();
         std::cout << ">Consensus LN:i:" << consensus.size() << std::endl
                   << consensus << std::endl;
@@ -177,6 +189,8 @@ void help() {
         "                0 - consensus\n"
         "                1 - multiple sequence alignment\n"
         "                2 - 0 & 1\n"
+        "        -f, --gfa\n"
+        "            write GFA on stdout\n"
         "        -d, --dot <file>\n"
         "            output file for the final POA graph in DOT format\n"
         "        --version\n"
